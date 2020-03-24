554f68a51bb4afb0dcacc8dcee01535ccac5f6a2
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-03-24 15:01:55 -0400

    Added note index view and note list item model

---
+++ ElevenNoteMVC/Controllers/NoteController.cs
@@ -0,0 +1,21 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNoteMVC.Controllers
+{
+    // This annotation makes it so that the views are accessible only to logged in users
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+    }
+}
