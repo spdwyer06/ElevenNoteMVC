7a0e2da293ad6365d399bdc17a8c4d6ac85bcc69
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-03-23 22:14:40 -0400

    Moved IdentityModels to Data layer

--- ElevenNoteMVC/App_Start/IdentityConfig.cs
+++ ElevenNoteMVC/App_Start/IdentityConfig.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using ElevenNoteMVC.Models;
+using ElevenNote.Data;
 
 namespace ElevenNoteMVC
 {
--- ElevenNoteMVC/Controllers/AccountController.cs
+++ ElevenNoteMVC/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using ElevenNoteMVC.Models;
+using ElevenNote.Data;
 
 namespace ElevenNoteMVC.Controllers
 {
