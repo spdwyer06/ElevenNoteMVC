21732bade625016d7b35e4efbfe0d744a71143ae
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-03-24 15:24:18 -0400

    Added note service

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,54 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userID;
+
+        public NoteService(Guid userID)
+        {
+            _userID = userID;
+        }
+
+        public bool CreateNote(NoteCreate model)
+        {
+            var entity = new Note()
+            {
+                OwnerID = _userID,
+                Title = model.Title,
+                Content = model.Content,
+                CreatedUtc = DateTimeOffset.Now
+            };
+
+            using (var dbContext = new ApplicationDbContext())
+            {
+                dbContext.Notes.Add(entity);
+                return dbContext.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            using (var dbContext = new ApplicationDbContext())
+            {
+                var query = dbContext.Notes
+                        .Where(x => x.OwnerID == _userID)
+                        .Select(x => new NoteListItem
+                        {
+                            NoteID = x.NoteID,
+                            Title = x.Title,
+                            CreatedUtc = x.CreatedUtc
+                        });
+
+                return query.ToArray();
+            }
+        }
+    }
+}
--- ElevenNoteMVC/Controllers/NoteController.cs
+++ ElevenNoteMVC/Controllers/NoteController.cs
@@ -1,4 +1,6 @@
 ﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +16,10 @@ namespace ElevenNoteMVC.Controllers
         // GET: /Note/Index
         public ActionResult Index()
         {
-            var model = new NoteListItem[0];
+            var userID = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userID);
+            var model = service.GetNotes();
+
             return View(model);
         }
 
@@ -29,11 +34,17 @@ namespace ElevenNoteMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-
+                return View(model);
             }
-            return View(model);
+
+            var userID = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userID);
+
+            service.CreateNote(model);
+
+            return RedirectToAction("Index");
         }
     }
 }
